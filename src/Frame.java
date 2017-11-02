f5ad6b89a03642f53df8623159e883a50332b28d
Ianpyrider <Ianpyrider@users.noreply.github.com>
2017-11-02 08:44:44 -0400

    Changed to system out print temporarily

--- src/Frame.java
+++ src/Frame.java
@@ -47,27 +47,27 @@ public class Frame implements ActionListener  {
 		{
 			int playerDamage = player1.attack();
 			
-			text.setText("\nPlayer attacks!");
-			text.setText(playerDamage + " damage done.");
+			System.out.println("\nPlayer attacks!");
+			System.out.println(playerDamage + " damage done.");
 			
 			if (playerDamage >= orc.getHealth()) {
-				text.setText("The orc is dead!");
+				System.out.println("The orc is dead!");
 				orc.damaged(playerDamage);
 			} else {
 				orc.damaged(playerDamage);
-				text.setText("Orc has " + orc.getHealth() + " health remaining.");
-				text.setText("The orc attacks!");
+				System.out.println("Orc has " + orc.getHealth() + " health remaining.");
+				System.out.println("The orc attacks!");
 				
 				int orcDamage = orc.attack();
 				
-				text.setText(orcDamage + " damage done.");
+				System.out.println(orcDamage + " damage done.");
 				
 				if (playerDamage >= player1.getHealth()) {
-					text.setText("The player is dead!");
+					System.out.println("The player is dead!");
 					player1.damaged(orcDamage);
 				} else {
 					player1.damaged(orcDamage);
-					text.setText("Player has " + player1.getHealth() + " health remaining.");
+					System.out.println("Player has " + player1.getHealth() + " health remaining.");
 				}
 			}
 		}
