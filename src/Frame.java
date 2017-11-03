992244c210c6bb673fa4587bab933ef255e9f86f
jimmyTheDukeOfCamelCase <33324950+jimmyTheDukeOfCamelCase@users.noreply.github.com>
2017-11-02 18:07:30 -0700

    Updated Weapon + Damage

--- src/Frame.java
+++ src/Frame.java
@@ -47,27 +47,27 @@ public class Frame implements ActionListener  {
 		{
 			int playerDamage = player1.attack();
 			
-			System.out.println("\nPlayer attacks!");
-			System.out.println(playerDamage + " damage done.");
+			text.setText("\nPlayer attacks!");
+			text.setText(playerDamage + " damage done.");
 			
 			if (playerDamage >= orc.getHealth()) {
-				System.out.println("The orc is dead!");
+				text.setText("The orc is dead!");
 				orc.damaged(playerDamage);
 			} else {
 				orc.damaged(playerDamage);
-				System.out.println("Orc has " + orc.getHealth() + " health remaining.");
-				System.out.println("The orc attacks!");
+				text.setText("Orc has " + orc.getHealth() + " health remaining.");
+				text.setText("The orc attacks!");
 				
 				int orcDamage = orc.attack();
 				
-				System.out.println(orcDamage + " damage done.");
+				text.setText(orcDamage + " damage done.");
 				
 				if (playerDamage >= player1.getHealth()) {
-					System.out.println("The player is dead!");
+					text.setText("The player is dead!");
 					player1.damaged(orcDamage);
 				} else {
 					player1.damaged(orcDamage);
-					System.out.println("Player has " + player1.getHealth() + " health remaining.");
+					text.setText("Player has " + player1.getHealth() + " health remaining.");
 				}
 			}
 		}
--- src/Playgame.java
+++ src/Playgame.java
@@ -1,4 +1,3 @@
-
 public class Playgame {
 
 	public Playgame() {
