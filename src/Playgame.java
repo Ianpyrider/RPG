de8fee6965c8a80f9ad21eabdd1af4d5a9c4c44b
jimmyTheDukeOfCamelCase <33324950+jimmyTheDukeOfCamelCase@users.noreply.github.com>
2017-11-07 06:43:26 -0800

    Wtf did i just do

---
+++ src/Playgame.java
@@ -0,0 +1,13 @@
+public class Playgame {
+
+	public Playgame() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Frame screen = new Frame();
+		
+	}
+
+}
---
+++ src/Weapon.java
@@ -0,0 +1,32 @@
+
+public class Weapon {
+	private int strength;
+	private int id;
+	private int level = 1;
+	
+	public Weapon(int weaponid) {
+		id = weaponid;
+		if (id == 0) {
+			//Fists
+			strength = 0;
+		} else if (id == 1) {
+			//Short sword
+			strength = 6;
+		} else if (id == 2) {
+			//Asgoroth's sword
+			strength = 100;
+		} else if (id==3) {
+			//Battle Hammer
+			strength = 8;
+		}
+	}
+	
+	public int getStrength()
+	{
+		return strength;
+	}
+	
+	public int getId() {
+		return id;
+	}
+}
